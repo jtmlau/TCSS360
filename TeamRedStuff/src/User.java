8f3a092eebcae9afd7eacda8059b911dbb1e53c0
AmandaAldrich <amlaldrich@gmail.com>
2017-04-12 21:03:51 -0700

    maybe, some basic code was written? But only if it commits. Added getters and setters to user, filled out basic constructor.

--- TeamRedStuff/src/User.java
+++ TeamRedStuff/src/User.java
@@ -1,16 +1,43 @@
 
 public class User {
 
-	private String email;
+	private String myEmail;
 	
-	private String firstName;
+	private String myFirstName;
 	
-	private String lastName;
+	private String myLastName;
 	
 	//User Constructor
-	public User (String firstName, String lastName, String email) {
+	public User (String theFirstName, String theLastName, String theEmail) {
 		//another comment to test pull requests 
 		//the needs of the many, outweigh the needs of the few -Spock
+		
+		myFirstName = theFirstName;
+		myLastName = theLastName;
+		myEmail = theEmail;
 	}
 	
+	public String getFirstName(){
+		return myFirstName;
+	}
+	
+	public String getLastName(){
+		return myLastName;
+	}
+	
+	public String getEmail(){
+		return myEmail;
+	}
+	
+	public void setFirstName(String newFirstName){
+		myFirstName = newFirstName;
+	}
+	
+	public void setLastName(String newLastName){
+		myLastName = newLastName;
+	}
+	
+	public void setEmail(String newEmail){
+		myEmail = newEmail;
+	}
 }
