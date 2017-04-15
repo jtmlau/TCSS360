9d711cc9e27071ef373686665a7eb86c8923bb7a
jimmy1b <best-jimmy@hotmail.com>
2017-04-15 12:48:39 -0700

    Jimmy - Added junit test files

---
+++ TeamRedStuff/src/tests/UserTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLastName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+}
